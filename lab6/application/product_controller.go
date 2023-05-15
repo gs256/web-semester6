85fcdf71ab526d5e3ab46db042722f05dadcc9c4
gs256 <regularnetuser@gmail.com>
2023-05-15 12:12:42 +0300

    Js render product details page

--- lab6/application/product_controller.go
+++ lab6/application/product_controller.go
@@ -2,9 +2,7 @@ package application
 
 import (
 	"lab6/products"
-	"lab6/utils"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,52 +14,10 @@ type ProductController struct {
 func (controller *ProductController) Initialize(engine *gin.Engine, service *products.ProductService) {
 	controller.service = service
 	engine.GET("/products", controller.indexRoute)
-	engine.GET("/products/:productId", controller.productDetailsRoute)
+	engine.GET("/products/:productId", controller.indexRoute)
 	engine.GET("/cart", controller.indexRoute)
 }
 
 func (controller *ProductController) indexRoute(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", nil)
-}
-
-func (controller *ProductController) productsRoute(c *gin.Context) {
-	p, err := controller.service.GetAllProducts()
-	productsOnPage := p[:]
-
-	page, err := strconv.Atoi(c.Query("page"))
-
-	if err != nil || page <= 0 {
-		page = 1
-	}
-
-	max, err := strconv.Atoi(c.Query("max"))
-
-	if err != nil {
-		max = len(productsOnPage)
-	}
-
-	maxPageNumber := products.MaxPageNumber(&productsOnPage, max)
-	page = utils.Clamp(page, 1, maxPageNumber)
-
-	productsOnPage = *products.ProductSlice(&productsOnPage, page, max)
-	c.HTML(http.StatusOK, "products.tmpl", gin.H{"products": productsOnPage, "page": page, "pageCount": maxPageNumber})
-}
-
-func (controller *ProductController) productDetailsRoute(c *gin.Context) {
-	productId := c.Param("productId")
-	product, err := controller.service.GetProductById(productId)
-
-	if err != nil {
-		return404(c)
-	} else {
-		c.HTML(http.StatusOK, "details.tmpl", product)
-	}
-}
-
-func (controller *ProductController) cartRoute(c *gin.Context) {
-	c.HTML(http.StatusOK, "cart.tmpl", nil)
-}
-
-func return404(c *gin.Context) {
-	c.JSON(http.StatusNotFound, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
 }
--- lab6/static/js/app/app.js
+++ lab6/static/js/app/app.js
@@ -1,5 +1,6 @@
 import { CartPage } from "./cart-page.js"
 import { HomePage } from "./home-page.js"
+import { ProductDetailsPage } from "./product-details-page.js"
 import { ProductPage } from "./product-page.js"
 
 const root = document.getElementById("root")
@@ -9,6 +10,8 @@ if (route === "/") {
     new HomePage(root).render()
 } else if (route == "/products") {
     new ProductPage(root).render()
+} else if (route.startsWith("/products/")) {
+    new ProductDetailsPage(root).render()
 } else if (route == "/cart") {
     new CartPage(root).render()
 }
