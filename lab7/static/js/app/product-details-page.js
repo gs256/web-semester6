c39dbbd478a71d01054337a000541dae07bfcab2
gs256 <regularnetuser@gmail.com>
2023-05-15 12:36:55 +0300

    Use api on product details page

--- lab7/static/js/app/product-details-page.js
+++ lab7/static/js/app/product-details-page.js
@@ -1,20 +1,25 @@
 import { appendCss } from "./page-utils.js"
 import { Navbar } from "./navbar.js"
+import { getProducts } from "./api.js"
 
 export class ProductDetailsPage {
     constructor(root) {
         this.root = root
     }
 
-    render() {
+    async render() {
+        const productId = window.location.pathname.split("/").at(-1)
+        const products = await getProducts()
+        const product = products.find(p => p.id == productId)
+
         const html = `
             <div class="page-content">
                 <div class="item-details">
-                    <img class="details-image img-thumbnail" src={{ .ImageUrl }}/>
-                    <h1 class="details-name">{{ .Name }}</h1>
-                    <h1 class="details-price">{{ .Price }}₽</h1>
+                    <img class="details-image img-thumbnail" src=${product.imageUrl}/>
+                    <h1 class="details-name">${product.name}</h1>
+                    <h1 class="details-price">${product.price}₽</h1>
                     <h1 class="details-description-header">Описание</h1>
-                    <p class="details-description">{{ .Description }}</p>
+                    <p class="details-description">${product.description}</p>
                 </div>
             </div>
         `
