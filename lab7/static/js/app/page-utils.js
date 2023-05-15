ee8cc5ad794ef69f5aed5586ac8b87c610ecd1ce
gs256 <regularnetuser@gmail.com>
2023-05-15 12:51:35 +0300

    Use api on admin page

--- lab7/static/js/app/page-utils.js
+++ lab7/static/js/app/page-utils.js
@@ -5,3 +5,9 @@ export function appendCss(filename) {
     fileref.setAttribute("href", filename)
     document.getElementsByTagName("head")[0].appendChild(fileref)
 }
+
+export function createElementFromHtml(html) {
+    var div = document.createElement("div")
+    div.innerHTML = html.trim()
+    return div.firstChild
+}
