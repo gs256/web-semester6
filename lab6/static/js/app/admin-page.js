4aee516729ee6d53f9b5d555588d6d2c4f3e3c87
gs256 <regularnetuser@gmail.com>
2023-05-16 20:35:37 +0300

    Add mock admin-page for lab6

--- lab6/static/js/app/admin-page.js
+++ lab6/static/js/app/admin-page.js
@@ -11,25 +11,41 @@ export class AdminPage {
             <div class="page-content">
                 <h1>Products</h1>
                 <div class="admin-product-list">
-                    {{range .products}}
-                        <div class="admin-product-item">
-                            <span class="admin-item-name">
-                                <span>{{ .Name }}</span>
-                            </span>
-                            <div class="admin-item__buttons">
-                                <a class="admin-item-edit link-button" href="/admin/products/edit/{{ .Id }}">Edit</a>
-                                <a class="admin-item-delete link-button" href="/admin/products/delete/{{ .Id }}">Delete</a>
-                            </div>
-                        </div>
-                    {{end}}
                 </div>
                 <a href="/admin/products/create">
-                    <button>Create</button>
+                    <button class="btn btn-primary">Create</button>
                 </a>
             </div>
         `
         new Navbar(this.root).render()
         this.root.insertAdjacentHTML("beforeend", html)
         appendCss("/static/css/admin.css")
+        this.populateItems()
+    }
+
+    populateItems() {
+        const list = document.querySelector(".admin-product-list")
+
+        for (let i = 0; i < 10; i++) {
+            list.appendChild(this.createMockItem())
+        }
+    }
+
+    createMockItem() {
+        const html = `
+            <div class="card admin-product-item">
+                <span class="admin-item-name">
+                    <span>Mock Item</span>
+                </span>
+                <div class="admin-item__buttons">
+                    <a class="admin-item-edit link-button" href="/admin/products/edit/#">Edit</a>
+                    <a class="admin-item-delete link-button" href="/admin/products/delete/#">Delete</a>
+                </div>
+            </div>
+        `
+
+        var div = document.createElement("div")
+        div.innerHTML = html.trim()
+        return div.firstChild
     }
 }
